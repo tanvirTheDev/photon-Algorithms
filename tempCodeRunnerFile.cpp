2494654843fb1eb9aca709f8560b1feed1f7b3a0
Tanvir Ahamed <ahamedtanvir374@gmail.com>
2025-10-10 21:43:04 +0600

    add module - 07 - dijstra

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,65 +1,9 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-using namespace std;
-
-
-char grid[105][105];
-bool vis[105][105];
-vector<pair<int, int>> d = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-int n,m;
-int ax, ay, bx, by; 
-bool valid(int i, int j){
-    if(i < 0 || i >= n || j < 0 || j >= m){
-        return false;
-    }
-    if(grid[i][j] == '#'){
-        return false;
-    } 
-    else {
-        return true;
-    }
-}
-
-void dfs(int si, int sj){
-    vis[si][sj] = true;
-
-    for (int i = 0; i < 4; i++)
+for (int i = 0; i < n; i++)
     {
-        int ci, cj;
-        ci = si +  d[i].first;
-        cj = sj + d[i].second;
-        if(valid(ci, cj) == true && !vis[ci][cj]){
-            dfs(ci, cj);
-        }
-    }
-    
-}
- 
-int main()
-{
-
-    
-    cin >> n >> m;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
+        cout << i << "-> ";
+        for (auto p : adj_list[i])
         {
-           cin >> grid[i][j];
-            if (grid[i][j] == 'A') {
-                ax = i; ay = j;
-            }
-            if (grid[i][j] == 'B') {
-                bx = i; by = j;
-            }
+            cout << p.first << " " << p.second << ", ";
         }
-        
+        cout << endl;
     }
-
-    dfs(ax, ay);
-
-    if(vis[bx][by]) cout << "YES";
-    else cout << "NO";
-    
-    return 0;
-}
